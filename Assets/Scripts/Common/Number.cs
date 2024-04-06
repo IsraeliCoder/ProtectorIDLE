f730dfe3496943fc5480d43d371edc404e5da7d9
Orleg01 <orleg01@gmail.com>
2024-04-06 09:29:30 +0300

    Equal not needed

--- Assets/Scripts/Common/Number.cs
+++ Assets/Scripts/Common/Number.cs
@@ -108,7 +108,7 @@ public class Number
             if (MainNumber == other.MainNumber)
             {
 
-                return SubNumber >= other.SubNumber;
+                return SubNumber > other.SubNumber;
 
             }
 
